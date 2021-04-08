239b97133c56f84f1aa36da7ff42032087fbaf8d
7antra <yann@reliefs.dev>
2021-04-08 19:02:01 +0200

    exit begin

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,16 +1,14 @@
-import { timestamp, build } from "$service-worker";
+import { timestamp, build } from '$service-worker';
 
 const name = `cache-${timestamp}`;
 
-console.log("build", build);
+console.log('build', build);
 
-self.addEventListener("install", (event) => {
-	event.waitUntil(
-		caches.open(name).then((cache) => cache.addAll(build))
-	);
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(name).then((cache) => cache.addAll(build)));
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
 	event.waitUntil(
 		caches.keys().then(async (keys) => {
 			for (const key of keys) {
@@ -20,32 +18,25 @@ self.addEventListener("activate", (event) => {
 	);
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
 	const { request } = event;
 
-	if (request.method !== "GET" || request.headers.has("range"))
-		return;
+	if (request.method !== 'GET' || request.headers.has('range')) return;
 
 	const url = new URL(request.url);
 	const cached = caches.match(request);
 
-	if (
-		url.origin === location.origin &&
-		build.includes(url.pathname)
-	) {
+	if (url.origin === location.origin && build.includes(url.pathname)) {
 		// always return build files from cache
 		event.respondWith(cached);
-	} else if (
-		url.protocol === "https:" ||
-		!!location.hostname.indexOf("apparelo")
-	) {
+	} else if (url.protocol === 'https:' || !!location.hostname.indexOf('apparelo')) {
 		// hit the network for everything else...
 		const promise = fetch(request);
 
 		// ...and cache successful responses...
 		promise.then((response) => {
 			// cache successful responses
-			if (response.ok && response.type === "basic") {
+			if (response.ok && response.type === 'basic') {
 				const clone = response.clone();
 				caches.open(name).then((cache) => {
 					cache.put(request, clone);
