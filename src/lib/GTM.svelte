e28d670149fe256345dd181050b9485a86bb47d3
7antra <yann@boug.art>
2021-04-09 11:19:08 +0200

    Gtm

---
+++ src/lib/GTM.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { browser } from '$app/env';
+	export let id;
+	if (browser) {
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', `${id}`);
+	}
+</script>
+
+<noscript>
+	<iframe
+		title="gtm"
+		src="https://www.googletagmanager.com/ns.html?id={id}"
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	/>
+</noscript>
--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,8 +1,11 @@
 <script>
+	import Gtm from '$lib/GTM.svelte';
 	import Seo from '$lib/SEO.svelte';
 
 	import '../app.scss';
 </script>
+
+<Gtm id="GTM-P7D7JW7" />
 
 <Seo />
 
