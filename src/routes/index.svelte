4042c3c8aa379ecb7d1850bf5a55f38da8b2a643
7antra <yann@boug.art>
2021-04-09 12:25:50 +0200

    intro

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+	import { quadOut } from 'svelte/easing';
+
 	let count: number = 100;
+	let intro = tweened(0, {
+		easing: quadOut,
+		duration: 1200
+	});
+
+	$: count = $intro;
+	onMount(() => {
+		intro.set(100);
+	});
 
 	const randomise = () => {
 		count = Math.random() * 100;
@@ -7,7 +20,7 @@
 </script>
 
 <main on:click={randomise}>
-	<h1>{~~count}</h1>
+	<h1 class:three={~~count.toString().length === 3}>{~~count}</h1>
 </main>
 
 <style lang="scss">
@@ -25,10 +38,15 @@
 			-moz-user-select: none;
 			-ms-user-select: none;
 			user-select: none;
+			transition: all 1s;
 
 			// @media (max-width: 600px) {
 			// 	font-size: 31.2vh;
 			// }
+
+			&.three {
+				font-size: 51.8vh;
+			}
 
 			@media screen and (max-aspect-ratio: 1/1) {
 				font-size: 31.2vh;
