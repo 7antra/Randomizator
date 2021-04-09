f224c3ca95afbbcc52e133aae2716d0e12046ee5
7antra <yann@boug.art>
2021-04-09 22:34:34 +0200

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,9 @@
 	import { tweened } from 'svelte/motion';
 	import { quadOut } from 'svelte/easing';
 
-	let count: number = 100;
+	// let arr = [];
+
+	let count: number = 0;
 	let intro = tweened(0, {
 		easing: quadOut,
 		duration: 1200
@@ -15,12 +17,29 @@
 	});
 
 	const randomise = () => {
-		count = Math.random() * 100;
+		count = Math.floor(Math.random() * 100) + 1;
 	};
+
+	// const test = () => {
+	// 	arr = [];
+	// 	for (let i = 0; i < 1000; i++) {
+	// 		randomise();
+	// 		arr.push(count);
+	// 	}
+	// 	console.log('arr : ', arr);
+	// };
+
+	// $: arr;
 </script>
 
 <main on:click={randomise}>
-	<h1 class:three={~~count.toString().length === 3}>{~~count}</h1>
+	<h1 class:three={count.toString().length === 3}>{~~count}</h1>
+	<!-- <button on:click={test}>test</button>
+	<div class="test">
+		{#each arr as x}
+			<div style={`grid-column: ${x - 1}/${x};`} />
+		{/each}
+	</div> -->
 </main>
 
 <style lang="scss">
@@ -52,5 +71,19 @@
 				font-size: 31.2vh;
 			}
 		}
+
+		// .test {
+		// 	display: grid;
+		// 	position: fixed;
+		// 	bottom: 2vh;
+		// 	left: 2vh;
+		// 	width: calc(100vw - 4vh);
+		// 	grid-template-columns: repeat(100, 1fr);
+
+		// 	div {
+		// 		height: 2px;
+		// 		background-color: red;
+		// 	}
+		// }
 	}
 </style>
